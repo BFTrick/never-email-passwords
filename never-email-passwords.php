8dfc1cad4c33bf015c52cf5778f7aab8abbc6632
Graham Christensen <gchristensen@nationalfield.org>
2013-02-19 17:06:55 -0500

    Brining in parethesis with consistent style.

--- never-email-passwords.php
+++ never-email-passwords.php
@@ -7,16 +7,18 @@
  * Description: Send new users a reset password link when their account is created.
  */
 
-add_action( 'user_register', 'nep_user_register' );
-add_action( 'admin_print_scripts', 'nep_remove_email_checkbox' );
+add_action('user_register', 'nep_user_register');
+add_action('admin_print_scripts', 'nep_remove_email_checkbox');
 
-function nep_user_register( $user_id ) {
+
+function nep_user_register($user_id)
+{
     global $wpdb;
 
-    $user_data = get_userdata( $user_id );
+    $user_data = get_userdata($user_id);
 
-    if ( is_wp_error($user_data) ) {
-        error_log( sprintf("NeverEmailPasswords:nep_user_register error from get_userdata( %s ): %s", $user_id, $user_data->get_error_message()) );
+    if (is_wp_error($user_data)) {
+        error_log(sprintf("NeverEmailPasswords:nep_user_register error from get_userdata( %s ): %s", $user_id, $user_data->get_error_message()));
         return false;
     }
 
@@ -28,25 +30,26 @@ function nep_user_register( $user_id ) {
 
     $blog_name = get_bloginfo('name');
     $subject = "Please set your $blog_name password";
-    $body = nep_message_body( $blog_name ) .
-        network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_data->user_login), 'login');
+    $body = nep_message_body($blog_name, network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_data->user_login), 'login'));
 
-    if ( ! wp_mail($user_data->user_email, $subject, $body) ) {
-        error_log( sprintf("Failed sending email to <$user_data->user_email>:\n$subject\n$body") );
+    if (!wp_mail($user_data->user_email, $subject, $body)) {
+        error_log(sprintf("Failed sending email to <$user_data->user_email>:\n$subject\n$body"));
         return false;
     }
 
-    error_log( sprintf("Sent password reset link to %s", $user_data->user_login) );
+    error_log(sprintf("Sent password reset link to %s", $user_data->user_login));
     return true;
 }
 
-function nep_remove_email_checkbox() {
-    $password = wp_generate_password( 64, false );
-    wp_enqueue_script( 'nep_remove_email_checkbox', plugins_url('/js/nep_remove_email_checkbox.js', __FILE__), array(), false, true );
-    wp_localize_script( 'nep_remove_email_checkbox', 'NeverEmailPasswords', array('password' => $password) );
+function nep_remove_email_checkbox()
+{
+    $password = wp_generate_password(64, false);
+    wp_enqueue_script('nep_remove_email_checkbox', plugins_url('/js/nep_remove_email_checkbox.js', __FILE__), array(), false, true);
+    wp_localize_script('nep_remove_email_checkbox', 'NeverEmailPasswords', array('password' => $password));
 }
 
-function nep_message_body( $blog_name ) {
+function nep_message_body($blog_name)
+{
     return <<<EOB
 An account has been created for you at $blog_name, you need to set a password
 for this account before it can be used.
